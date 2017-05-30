a1940d4a9ebfe611961c656ccdaa83ed1ff00247
alinacorobea <alina.corobea@gmail.com>
2017-05-30 23:16:31 +0100

    Some config files

---
+++ com/edocti/jnext/gof/game/ProjectileFactory.java
@@ -0,0 +1,12 @@
+package com.edocti.jnext.gof.game;
+
+/**
+ * Created by Adam on 21/05/2017.
+ */
+public class ProjectileFactory {
+    public enum PowerLevel { WEAK, DAMAGING, DEVASTATING }
+
+    public Projectile createProjectile() {
+
+    }
+}
