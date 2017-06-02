3efaca3b23b2d8d42a5db7594947f4b9177544f9
alinacorobea <alina.corobea@gmail.com>
2017-06-02 20:52:24 +0100

    Added ProjectileFactory

--- com/edocti/jnext/gof/game/ProjectileFactory.java
+++ com/edocti/jnext/gof/game/ProjectileFactory.java
@@ -1,12 +1,9 @@
 package com.edocti.jnext.gof.game;
 
-/**
- * Created by Adam on 21/05/2017.
- */
 public class ProjectileFactory {
     public enum PowerLevel { WEAK, DAMAGING, DEVASTATING }
 
     public Projectile createProjectile() {
-
+        return null;
     }
 }
